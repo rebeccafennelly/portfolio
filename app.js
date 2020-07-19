5237adc123046d50896a354c80ae9f68536c950b
rebeccafennelly <64370782+rebeccafennelly@users.noreply.github.com>
2020-07-19 17:44:12 +0100

    tweaking chevrons and small changes

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
-menuBtn.addEventListener ('click',() => {
+menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
-    menuBtn.classList.add('open');
+    menuBtn.classList.add("open");
     menuOpen = true;
   } else {
-    menuBtn.classList.remove('open');
+    menuBtn.classList.remove("open");
     menuOpen = false;
   }
   toggleMenu();
 });
 
 const toggleMenu = () => {
-  let mainNav= document.getElementsByClassName('hide')[0];
+  let mainNav = document.getElementsByClassName("hide")[0];
   console.log(mainNav);
-  if (mainNav.classList.contains ('show')) {
-    mainNav.classList.remove ('show');
+  if (mainNav.classList.contains("show")) {
+    mainNav.classList.remove("show");
   } else {
-    mainNav.classList.add('show');
+    mainNav.classList.add("show");
   }
-}
+};
 
-window.onscroll = scrollFunction();
+// window.onscroll = scrollFunction();
 
-const scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById(".bexface").style.width = "125px";
-  } else {
-    document.getElementById("header").style.fontSize = "250px";
-  }
-}
+// const scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById(".bexface").style.width = "125px";
+//   } else {
+//     document.getElementById("header").style.fontSize = "250px";
+//   }
+// }
 // toggleMenu();
-;
 
+const bexfaceImage = document.querySelector(".bexface");
+console.log(bexfaceImage);
 
-
-
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 300 && !bexfaceImage.classList.contains("shrink")) {
+    bexfaceImage.classList.add("shrink");
+  }
+  else if (window.pageYOffset <= 300 && bexfaceImage.classList.contains("shrink")) {
+    bexfaceImage.classList.remove("shrink");
+}
+  });
